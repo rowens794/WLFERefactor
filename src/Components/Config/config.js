7fe2eae2fbdea25e6e4437c05bf05b1fd806ea0a
Ryan <rowens794@gmail.com>
2019-03-23 10:27:21 -0400

    added google tag manager

--- src/Components/Config/config.js
+++ src/Components/Config/config.js
@@ -4,7 +4,7 @@
 //backendRootURL: 'http://localhost:3001'
 
 const config = {
-  backendRootURL: 'https://fast-chamber-60180.herokuapp.com',
+  backendRootURL: 'http://localhost:3001',
 };
 
 export default config;
